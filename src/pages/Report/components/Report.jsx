dee8cb672b747168dfd054ca166a2614f4905f79
Edward <edward.nayve@boom.ai>
2024-10-07 11:29:38 +0800

    Apply memo in report list component

--- src/pages/Report/components/Report.jsx
+++ src/pages/Report/components/Report.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 import dayjs from 'dayjs';
@@ -17,47 +17,13 @@ import { makeStyles } from '@mui/material';
 // import StepLabel from '@mui/material/StepLabel';
 
 import { reportData } from '../../../constants';
+import ReportList from './ReportList';
 import { FormModal } from '../../../shared/FormModal';
 import { getCurrentDate, postRequest, dateFormat, addDays } from '../../../utils';
 
 
 import { useSelector, useDispatch } from 'react-redux';
 import { getUserFetch } from '../../../actions/userAction';
-
-
-function ReportList({ data }) {
-
-
-  return (
-    <ul role="list" className="divide-y divide-gray-100">
-      {data.map((item) => {
-        const amount = `${item?.amount.toLocaleString()}.00`
-        return (
-          <li className="flex justify-between gap-x-6 py-5" key={item.name}>
-            <div className="flex min-w-0 gap-x-4 items-center">
-              <span>
-                <MonetizationOnIcon fontSize='large' />
-              </span>
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{item.name}</p>
-              </div>
-            </div>
-            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm font-medium leading-6 text-gray-900">
-                <dt className="text-center font-extrabold truncate">
-                  {amount}
-                </dt>
-              </p>
-              <p className="mt-1 text-xs leading-5 text-gray-500">{dateFormat(item?.date_created)}</p>
-            </div>
-          </li>
-        )
-      })}
-
-    </ul>
-  )
-}
-
 
 function StepOne() {
   return (
---
+++ src/pages/Report/components/ReportList.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+
+
+import { dateFormat } from '../../../utils';
+
+const ReportList = ({ data }) => {
+    console.log("Rerendering ....")
+    return (
+        <ul role="list" className="divide-y divide-gray-100">
+            {data.map((item) => {
+                const amount = `${item?.amount.toLocaleString()}.00`
+                return (
+                    <li className="flex justify-between gap-x-6 py-5" key={item.name}>
+                        <div className="flex min-w-0 gap-x-4 items-center">
+                            <span>
+                                <MonetizationOnIcon fontSize='large' />
+                            </span>
+                            <div className="min-w-0 flex-auto">
+                                <p className="text-sm font-semibold leading-6 text-gray-900">{item.name}</p>
+                            </div>
+                        </div>
+                        <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                            <p className="text-sm font-medium leading-6 text-gray-900">
+                                <dt className="text-center font-extrabold truncate">
+                                    {amount}
+                                </dt>
+                            </p>
+                            <p className="mt-1 text-xs leading-5 text-gray-500">{dateFormat(item?.date_created)}</p>
+                        </div>
+                    </li>
+                )
+            })}
+
+        </ul>
+    )
+}
+
+export default React.memo(ReportList)
