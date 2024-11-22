ac838d065265d6043c3dd67eae223fb6f78002bf
Edward <edward.nayve@boom.ai>
2024-11-22 17:03:32 +0800

    Update UI for create record modal

--- src/shared/FormModal.jsx
+++ src/shared/FormModal.jsx
@@ -15,13 +15,15 @@ const style = {
   borderRadius: 2,
 };
 
-export function FormModal({ children, open, handleClose }){
+export function FormModal({ children, open, handleClose }) {
   return (
     <>
       <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          {children}
-        </Box>
+        <div id="crud-modal" tabIndex="-1" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div style={style}>
+            {children}
+          </div>
+        </div>
       </Modal>
     </>
   )
